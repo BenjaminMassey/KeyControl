ac3808701874cac4ecd6b46fefe212ad82524230
BenjaminMassey <benjamin.w.massey@gmail.com>
2020-07-10 14:51:57 -0700

    First commit

---
+++ Assets/Script/CitizenClamour.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CitizenClamour : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine("Clamour");
+    }
+
+    // Update is called once per frame
+    IEnumerator Clamour()
+    {
+        Vector2 start = transform.position;
+        float bottom = start.y - Random.Range(0.05f, 0.2f);
+        float top = start.y + Random.Range(0.05f, 0.2f);
+
+        float amount = 3 / 50.0f;
+        bool up = true;
+        float t = 0.0f;
+        while (true)
+        {
+            if (up) { t += amount;  }
+            else { t -= amount; }
+            float loc = (1.0f - t) * bottom + top * t;
+            if (t >= 1.0f) { up = false; }
+            if (t <= 0.0f) { up = true; }
+            transform.position = new Vector2(start.x, loc);
+            yield return new WaitForFixedUpdate();
+        }
+    }
+}
---
+++ Assets/Script/GenerateCitizens.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GenerateCitizens : MonoBehaviour
+{
+    public Sprite citizen_base;
+
+    private int numCitizens;
+
+    private GameObject[] citizens;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        numCitizens = 25;
+
+        for (int i = 0; i < numCitizens; i++)
+        {
+            Vector2 spawn = new Vector2();
+            spawn.x = transform.position.x + Random.Range(-8.0f, 8.0f);
+            spawn.y = transform.position.y + Random.Range(-0.5f, 0.5f);
+            GameObject go = new GameObject();
+            go.AddComponent<SpriteRenderer>();
+            go.GetComponent<SpriteRenderer>().sprite = citizen_base;
+            go.transform.position = spawn;
+            go.transform.rotation = Quaternion.identity;
+            go.AddComponent<CitizenClamour>();
+            go.transform.parent = transform;
+        }
+    }
+}
