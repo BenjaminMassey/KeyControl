f1bbcc8dafeae7081976b19435047921cc11bf73
BenjaminMassey <benjamin.w.massey@gmail.com>
2020-07-11 00:37:53 -0700

    Have lerped scrolling with switched side, and added arrows to buttons

--- Assets/Scripts/SideHandler.cs
+++ Assets/Scripts/SideHandler.cs
@@ -20,24 +20,41 @@ public class SideHandler : MonoBehaviour
     private void ShowHideSide()
     {
         left_side.SetActive(!MainValues.onRightSide);
+        GameObject.Find("STUPIDLEFTARROW").GetComponent<Text>().enabled = MainValues.onRightSide;
         right_side.SetActive(MainValues.onRightSide);
+        
     }
 
     public void ToggleSide()
     {
+        StartCoroutine("SideChange");
+    }
+
+    private IEnumerator SideChange()
+    {
+        left_side.SetActive(false);
+        GameObject.Find("STUPIDLEFTARROW").GetComponent<Text>().enabled = false;
+        right_side.SetActive(false);
+
         GameObject p = transform.parent.gameObject;
         Vector3 pos = p.transform.position;
-        if (MainValues.onRightSide)
+        float amount = 0.06f;
+        float a = 0.0f;
+        float b = 17.25f;
+        float t;
+        if (MainValues.onRightSide) { t = 1.0f; }
+        else { t = 0.0f; }
+        while (true)
         {
-            p.transform.position = new Vector3(0.0f, pos.y, pos.z);
+            if (MainValues.onRightSide) { t -= amount; }
+            else { t += amount; }
+            float loc = (1.0f - t) * a + b * t;
+            if (t >= 1.0f || t <= 0.0f ) { break; }
+            p.transform.position = new Vector3(loc, pos.y, pos.z);
+            yield return new WaitForFixedUpdate();
         }
-        else
-        {
-            p.transform.position = new Vector3(17.25f, pos.y, pos.z);
-        }
-
-        MainValues.onRightSide = !MainValues.onRightSide;
         
+        MainValues.onRightSide = !MainValues.onRightSide;
         ShowHideSide();
     }
 }
